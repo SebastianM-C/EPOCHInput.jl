1c9e370570f758c220d3605a4266d64f442b749f
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-01-19 14:20:35 +0200

    Add code

--- src/EPOCHInput.jl
+++ src/EPOCHInput.jl
@@ -1,5 +1,14 @@
 module EPOCHInput
 
-# Write your package code here.
+using Unitful
+using PhysicalConstants.CODATA2018: c_0, ε_0, μ_0, m_e, e
+using BangBang
+
+include("parse.jl")
+include("constant.jl")
+include("control.jl")
+include("no_globals.jl")
+include("no_parsing.jl")
+include("output.jl")
 
 end
