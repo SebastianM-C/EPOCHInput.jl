56ad1461e0ef28520f7909294252f8edc2316e7f
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-01-20 13:54:47 +0200

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,47 @@ using Test
 using Unitful
 
 @testset "EPOCHInput.jl" begin
-    p = parse_input("gauss/input.deck")
-    @test p[:nx] == 10
-    @test p[:a0] == 70
-    @test p[:laser].lambda ≈ 800u"nm"
+    folders = [
+        "gauss",
+        "Nuter2018",
+        "Wang-Arefiev2020"
+    ]
+    results = Dict(
+        "gauss" => Dict(
+            :nx => 10,
+            :a0 => 70,
+            :laser => (
+                lambda = 800u"nm",
+            )
+        ),
+        "Nuter2018" => Dict(
+            :nx => 640,
+            :a0 => 1,
+            :laser => (
+                lambda = 800u"nm",
+            )
+        ),
+        "Wang-Arefiev2020" => Dict(
+            :a0 => 85,
+            :laser => (
+                lambda = 1u"μm",
+            )
+        )
+    )
+    @testset "$folder" for folder in folders
+        p = parse_input("$folder/input.deck")
+        @testset "$k" for (k,v) in results[folder]
+            if v isa NamedTuple
+                for (_k, _v) in pairs(v)
+                    if unit(_v) == NoUnits
+                        @test getproperty(p[k], _k) == _v
+                    else
+                        @test getproperty(p[k], _k) ≈ _v
+                    end
+                end
+            else
+                @test p[k] == v
+            end
+        end
+    end
 end
