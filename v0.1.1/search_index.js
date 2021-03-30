ad4410dcaa766f321fe1cdf2e5a03764602bbb1a
Documenter.jl <documenter@juliadocs.github.io>
2021-03-30 18:53:39 +0000

    build based on f4c1b36

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EPOCHInput","category":"page"},{"location":"#EPOCHInput","page":"Home","title":"EPOCHInput","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EPOCHInput]","category":"page"}]
+}
