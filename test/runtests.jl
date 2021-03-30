f4c1b369ffbc9cab10d0f7adcb72e3aa87c6278d
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-03-30 21:09:56 +0300

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,33 +12,33 @@ using Unitful
         "gauss" => Dict(
             :nx => 10,
             :a0 => 70,
-            :laser => (
-                lambda = 800u"nm",
+            :laser => Dict(
+                :lambda => 800u"nm",
             )
         ),
         "Nuter2018" => Dict(
             :nx => 640,
             :a0 => 1,
-            :laser => (
-                lambda = 800u"nm",
+            :laser => Dict(
+                :lambda => 800u"nm",
             )
         ),
         "Wang-Arefiev2020" => Dict(
             :a0 => 85,
-            :laser => (
-                lambda = 1u"μm",
+            :laser => Dict(
+                :lambda => 1u"μm",
             )
         )
     )
     @testset "$folder" for folder in folders
         p = parse_input("$folder/input.deck")
         @testset "$k" for (k,v) in results[folder]
-            if v isa NamedTuple
+            if v isa Dict
                 for (_k, _v) in pairs(v)
                     if unit(_v) == NoUnits
-                        @test getproperty(p[k], _k) == _v
+                        @test getindex(p[k], _k) == _v
                     else
-                        @test getproperty(p[k], _k) ≈ _v
+                        @test getindex(p[k], _k) ≈ _v
                     end
                 end
             else
